74019ec9c29d72ea1bb93e24cbbf565b34482647
HongBeen <redbean-2@naver.com>
2024-11-21 15:25:26 +0900

    setup swagger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
-@Controller()
+@Controller('status')
+@ApiTags('Status')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Get status' }) // API의 요약 정보
+  @ApiResponse({ status: 200, description: 'Success' }) // 응답 상태에 대한 설명
   getHello(): string {
     return this.appService.getHello();
   }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config'; 
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService) // DI(컨테이너, 의존성을 포함하여 완성된 객체를 만들어 반환하는 Nestjs 공장에 비유. 필요한 의존성을 스스로 찾고 조립.)
   const port = configService.get("port")
+
+  // Swagger 설정
+  const config = new DocumentBuilder()
+    .setTitle('Snow Step API Documentation') // API 문서 제목
+    .setDescription('API endpoints for our application') // 설명
+    .setVersion('1.0') // 버전
+    .addBearerAuth() // JWT Bearer 인증 추가
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // '/api' 경로에서 Swagger UI 제공
 
   await app.listen(port, () => console.log(`Server is listening! Port is ${port}`));
 }
