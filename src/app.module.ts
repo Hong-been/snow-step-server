942c69526976355b3d58b0fb233b0f70d7431c09
HongBeen <redbean-2@naver.com>
2024-11-21 14:59:06 +0900

    add comments

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { config } from "src/config/config"
 import { ConfigModule } from '@nestjs/config'; 
 
+/**
+ * 모듈에서 컨트롤러와 서비스를 연결한다.
+ * imports: 다른 모듈을 가져옴
+ * controllers: 이 모듈에서 사용하는 컨트롤러 등록. 클라이언트 요청을 받는다.
+ * providers: 의존성 주입을 위해 서비스 등록. 컨트롤러에서 이 서비스를 사용해서 비지니스 로직을 처리한다. 서비스는 독립적으로 동작할 수 있게 설계해야하고, 컨트롤러는 서비스에 의존한다.
+ */
 @Module({
   imports: [
+    // https://dev.to/gmdias727/how-to-set-up-env-variables-in-a-nestjs-project-1o25
     ConfigModule.forRoot({
       isGlobal: true,
       load: [config],
--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,9 @@ import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService)
+  const configService = app.get(ConfigService) // DI(컨테이너, 의존성을 포함하여 완성된 객체를 만들어 반환하는 Nestjs 공장에 비유. 필요한 의존성을 스스로 찾고 조립.)
   const port = configService.get("port")
+
   await app.listen(port, () => console.log(`Server is listening! Port is ${port}`));
 }
 bootstrap();
