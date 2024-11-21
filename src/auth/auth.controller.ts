d436423cdccc0e77a1c8ae8fb09b5dc752557e4b
HongBeen <redbean-2@naver.com>
2024-11-21 21:54:44 +0900

    feat: google redirection

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Auth')
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  @Get('google')
+  @UseGuards(AuthGuard("google"))
+  @ApiOperation({summary: "Google Login", description: "Redirects to Google OAuth login page"})
+  @ApiResponse({ status: 302, description: 'Redirects to Google OAuth login page.' }) // 리디렉션 응답
+  googleLogin(): void{
+    // Passport가 구글의 OAuth 2.0 로그인 페이지로 사용자를 리디렉션.
+    // 리디렉션 URL은 GoogleStrategy의 설정값(clientID, callbackURL)에 의해 결정.
+  }
+}
