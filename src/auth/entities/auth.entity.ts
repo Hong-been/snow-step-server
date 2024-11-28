74699f15fc5caa379ff4d53ab652a7319fcfde77
HongBeen <redbean-2@naver.com>
2024-11-28 16:03:13 +0900

    chore: code review

--- src/auth/entities/auth.entity.ts
+++ src/auth/entities/auth.entity.ts
@@ -1,7 +1,6 @@
 import { Mail } from 'src/mails/entities/mail.entity';
 import { Step } from 'src/steps/entities/step.entity';
 import {
-  BaseEntity,
   Column,
   CreateDateColumn,
   Entity,
@@ -11,17 +10,18 @@ import {
 } from 'typeorm';
 
 @Entity()
-@Unique(['email', 'userName'])
-export class User extends BaseEntity {
+@Unique(['email', 'username'])
+export class User {
+  // BaseEntity를 상속받으면 TypeORM의 Active Record 패턴이 적용되는데, NestJS에서는 Repository 패턴을 사용하는 경우가 더 일반적입니다.
+  // 만약 Repository 패턴을 사용 중이라면 BaseEntity를 제거하고 단순히 엔티티 클래스로 작성하는 것이 권장됩니다.
   @PrimaryGeneratedColumn()
   id: number;
 
   @Column({ length: 255, unique: true })
   email: string;
 
-  // ${userName}@snowstep.com 으로 사용
   @Column({ length: 255, unique: true })
-  userName: string;
+  username: string;
 
   @Column({ length: 255 })
   firstName: string;
@@ -29,15 +29,16 @@ export class User extends BaseEntity {
   @Column({ length: 255 })
   lastName: string;
 
+  // TypeORM에서 nullable: true를 설정하면 string | null을 명시하지 않아도 됩니다.
   @Column({ length: 255, nullable: true })
-  picture: string | null;
+  picture: string;
 
   @CreateDateColumn()
   createdAt: Date;
 
-  @OneToMany((type) => Mail, (mail) => mail.user)
+  @OneToMany(() => Mail, (mail) => mail.user)
   mails: Mail[];
 
-  @OneToMany((type) => Step, (step) => step.user)
+  @OneToMany(() => Step, (step) => step.user)
   steps: Step[];
 }
